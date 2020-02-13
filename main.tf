b0b9102553f7f5c18691db4307f45bf4d3ceef57
Brandon LeBlanc <brandon@leblanc.codes>
2020-02-12 18:11:03 -0600

    remove kubernetes provider for now

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@ provider "aws" {
   version = "~> 2.48"
 }
 
-provider "kubernetes" {
-  version          = "~> 1.10"
-  load_config_file = false
-
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+//provider "kubernetes" {
+//  version          = "~> 1.10"
+//  load_config_file = false
+//
+//  host                   = data.aws_eks_cluster.cluster.endpoint
+//  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+//  token                  = data.aws_eks_cluster_auth.cluster.token
+//}
 
 locals {
   common-tags = {
